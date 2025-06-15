f1fd5ef505679553a08f3a04ca7214019e977afa
boyfachruri <boyfachruri@gmail.com>
2025-06-15 15:08:09 +0700

    disabled eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Nonaktifkan error ESLint saat build
+  },
+  // opsi lain bisa kamu tambahkan di sini
 };
 
 export default nextConfig;
